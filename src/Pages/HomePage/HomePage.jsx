36759070f483b76063df696ef22a6f954bb52233
Công Tú Trần <congtutran2k2@gmail.com>
2024-03-12 23:23:11 +0700

    12/03/2024

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from "react-slick";
+import Form from 'react-bootstrap/Form';
+import DatePicker from 'react-datepicker';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import 'react-datepicker/dist/react-datepicker.css';
+import Button from 'react-bootstrap/Button';
 
 const HomePage = () => {
+    
+    const [selectedDateIn, setSelectedDateIn] = useState(null);
+    const [selectedDateOut, setSelectedDateOut] = useState(null);
+    const handleChangeIn = (date) => {
+        setSelectedDateIn(date);
+    };const handleChangeOut = (date) => {
+        setSelectedDateOut(date);
+    };
     const sliders = [
         'https://khachsan3.migoda.vn/wp-content/uploads/2019/11/CortonaInn-AboutUs-Hero-5bf5845edceef.jpg',
         'https://khachsan3.migoda.vn/wp-content/uploads/2019/11/slider-2.jpg',
@@ -35,10 +47,54 @@ const HomePage = () => {
                 <Slider {...settings}></Slider>
             </div>
             <div className='grid grid-cols-6 w-full h-24 bg-[#000] mt-[-5.6px]'>
-                <div className='text-white font-bold'>
-                    <h1 >
+                <div className='text-white font-bold py-[17px] px-[15px] text-center'>
+                    <h1 className='text-2xl'>
                     Đặt phòng ngay
                     </h1>
+                    <span>Ưu đãi giá thấp nhất</span>
+                </div>
+                <div className='justify-center px-[15px] bg-goldlg '>
+                    <Form.Select aria-label="Default select example" className='text-center h-10 w-[224px] mt-[25px]' >
+                        <option>Người lớn</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="3">3</option>
+                        <option value="3">3</option>
+                        <option value="3">3</option>
+                    </Form.Select>
+                </div>
+                <div className='justify-center px-[15px] bg-goldlg '>
+                    <DatePicker
+                        selected={selectedDateIn}
+                        onChange={handleChangeIn}
+                        dateFormat="dd/MM/yyyy" // Định dạng ngày tháng
+                        placeholderText="Ngày đến"
+                        className='text-center h-10 w-[224px] mt-[25px]'
+                    />
+                </div>
+                <div className='justify-center px-[15px] bg-goldlg '>
+                    <DatePicker
+                        selected={selectedDateOut}
+                        onChange={handleChangeOut}
+                        dateFormat="dd/MM/yyyy" // Định dạng ngày tháng
+                        placeholderText="Ngày đi"
+                        className='text-center h-10 w-[224px] mt-[25px]'
+                    />
+                </div>
+                <div className='justify-center px-[15px] bg-goldlg'>
+                    <Form.Select aria-label="Default select example" className='text-center h-10 w-[224px] mt-[25px]' >
+                        <option>Trẻ em</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="3">3</option>
+                        <option value="3">3</option>
+                        <option value="3">3</option>
+                    </Form.Select>
+                </div>
+                <div className='justify-center px-[15px] bg-goldlg text-white items-center text-center '>
+                    <Button variant="outline-secondary" className='bg-[#755727] h-[96px] px-12'>KIỂM TRA</Button>
                 </div>
             </div>
         </div>
