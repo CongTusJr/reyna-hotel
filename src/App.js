c573edd04dc105158e0ddd84c75968281752af18
Công Tú Trần <congtutran2k2@gmail.com>
2024-03-17 10:04:44 +0700

    1111

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { routes } from "./routes";
+import { routes, routes1 } from "./routes";
 import Default from "./components/DefaultCpn/Default";
+import DefaultAD from "./components/DefaultCpn/DefaultAD";
 
 function App() {
   return (
@@ -16,6 +17,21 @@ function App() {
               <Route
                 key={route.path}
                 path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+          {routes1.map((route1) => {
+            const Page = route1.page;
+            const Layout = route1.isShowHeader ? DefaultAD : Fragment;
+            return (
+              <Route
+                key={route1.path}
+                path={route1.path}
                 element={
                   <Layout>
                     <Page />
